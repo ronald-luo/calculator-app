537ec768c22003195b4bf7ce6043dcb7ea9ccba3
ronald-luo <r.luo.180@gmail.com>
2021-06-01 17:19:21 -0400

    create basic calculator design

---
+++ index.js
@@ -0,0 +1,19 @@
+function add (numA, numB) {
+    return numA + numB
+}
+
+function subtract (numA, numB) {
+    return numA - numB
+}
+
+function multiply (numA, numB) {
+    return numA * numB
+}
+
+function divide (numA, numB) {
+    return numA / numB
+}
+
+function operate(numA, numB, operator) {
+    return operator(numA, numB)
+}
